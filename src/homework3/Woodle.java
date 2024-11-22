2aa988e22128553c5f19769bdfc86c5fe935a25c
ardenbijou <snavelya@wlhs.wlwv.k12.or.us>
2024-11-22 14:29:22 -0800

    just a start

---
+++ src/homework3/Woodle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+package homework3;
+
+public class Woodle {
+    public static void main(String[]args) {
+
+        Scanner input = new Scanner;
+        String word = ("nerds");
+        System.out.println("enter guesses in all caps");
+
+        System.out.print("guess a 5 letter word:");
+
+
+    }
+}
